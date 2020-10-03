2b6f528b1933da483524d3e1194bc480869aae0f
jardielson <jardielsonbio@gmail.com>
2020-10-03 12:34:09 -0300

    create files of connection with database , query and creates in the database, working perfectly

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,15 +1,16 @@
-const users =[
-    {id:1,name:"jardielson silva ferreira",email:"jardielsonbio@gmail.com",phone:"(86)9 8121-7085"},
-    {id:2,name:"sir isaac newton",email:"bestphysics@gmail.com",phone:"(xx) x xxxx-xxxx"},
-]
+const User = require("./User");
 
 module.exports = {
     Query:{
-        users: ()=>users,
-        user:  ()=>users[0]
+        users: ()=>User.find(),
+        user:  (_,{ id })=>User.findById(id),
     },
 
     Mutation:{
-        createUser: ()=>users[1],
+        createUser: (_,{name,email,phone})=>User.create({
+            name,
+            email,
+            phone,
+        })
     }
 }
--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const {GraphQLServer} =require('graphql-yoga');
 const path = require('path');
 const resolvers = require("./resolvers");
+const mongoose = require('mongoose');
 
-
+mongoose.connect('mongodb://localhost:27017/mongodb',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+});
 const server = new GraphQLServer({
     typeDefs: path.resolve(__dirname, 'schema.graphql'),
     resolvers
